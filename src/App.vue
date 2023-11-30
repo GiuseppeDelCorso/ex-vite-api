9fcab42e33905f8f6510901a747b7233bb1765b0
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-30 17:24:11 +0100

    fine primo esercizio

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+import InfoBeer from './components/InfoBeer.vue'
+import axios from 'axios';
+import { store } from "./store.js"
+
+export default {
+      components: {
+        InfoBeer
+      },
+      data() {
+		      return {
+			        store,
+		}
+	},
+  mounted() {
+
+		  this.prova();
+	},
+  methods:{
+      prova() {
+        axios.get(this.store.apiUrl).then(risultato => {
+
+          this.store.caratteristiche = risultato.data
+          console.log(risultato.data[0].name)
+        }) 
+
+        
+      }
+
+  }
+
+
+
+}
+</script>
+
+<template>
+  
+  <InfoBeer v-for="caratteristica in store.caratteristiche" :info="caratteristica" />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/InfoBeer.vue
@@ -0,0 +1,38 @@
+<script >
+
+export default {
+  name: "InfoBeer",
+  props: {
+
+    info: Object
+
+  }
+
+
+}
+
+
+
+
+</script>
+
+<template>
+    <div >
+      <hr class="spazio">
+      <p>Nome bar = {{info.name}}</p>
+      <p>Tipologia = {{ info.brewery_type }}</p>
+      <p>address = {{ info.address_1 }}</p>
+      <p>Citta = {{ info.city }}</p>
+      <p>Paese = {{ info.country }}</p>
+      <p>Codice Postale = {{ info.postal_code }}</p>
+    </div>
+
+
+
+</template>
+
+<style scoped>
+  .spazio{
+    margin: 50px 0px 50px 0px;
+  }
+</style>
